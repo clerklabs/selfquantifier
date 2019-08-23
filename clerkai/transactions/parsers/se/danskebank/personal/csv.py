03eb11ffbac1a4ba5bde92022b3bd69dde6d2109
Fredrik Wollsén <fredrik@neam.se>
2019-08-23 15:00:33 +0300

    Added danskebank.se parser

---
+++ clerkai/transactions/parsers/se/danskebank/personal/csv.py
@@ -0,0 +1,34 @@
+import datetime
+
+import pandas as pd
+
+from clerkai.transactions.parsers.parse_utils import amount_to_rounded_decimal
+
+
+def ymd_date_to_datetime_obj(datetime_str):
+    datetime_obj = datetime.datetime.strptime(datetime_str, '%Y-%m-%d')
+    return datetime_obj
+
+
+def import_danskebank_se_csv_transaction_file(transaction_file):
+    return pd.read_csv(transaction_file, encoding="iso8859_15", sep=";", decimal=",", quotechar="\"", thousands=".")
+
+
+def danskebank_se_csv_transactions_to_general_clerk_format(df):
+    normalized_df = pd.DataFrame()
+
+    normalized_df['Date'] = df['Bokföringsdag'].apply(ymd_date_to_datetime_obj)
+    normalized_df['Payee'] = None
+    normalized_df['Memo'] = df['Specifikation']
+    normalized_df['Amount'] = df['Belopp'].apply(amount_to_rounded_decimal)
+    normalized_df['Balance'] = df['Saldo'].apply(amount_to_rounded_decimal)
+
+    normalized_df['Original data'] = df[
+        ['Bokföringsdag', 'Specifikation', 'Belopp', 'Saldo', 'Status', 'Avstämt']].to_dict(
+        orient='records')
+    return normalized_df[['Date', 'Payee', 'Memo', 'Amount', 'Balance', 'Original data']]
+
+
+def danskebank_se_csv_transactions_parser(transaction_file):
+    df = import_danskebank_se_csv_transaction_file(transaction_file)
+    return danskebank_se_csv_transactions_to_general_clerk_format(df)
---
+++ clerkai/transactions/parsers/se/danskebank/personal/test_csv.py
@@ -0,0 +1,13 @@
+from os.path import dirname, join, realpath
+
+from clerkai.transactions.parsers.se.danskebank.personal.csv import \
+    danskebank_se_csv_transactions_parser
+
+test_data_dir_path = join(dirname(realpath(__file__)), "test_data")
+
+
+def test_danskebank_se_csv_transactions_parser():
+    transaction_file_path = join(test_data_dir_path, 'Transaktioner-12345678901-20190821.edited.csv')
+    transactions_df = danskebank_se_csv_transactions_parser(
+        transaction_file_path)
+    assert not transactions_df.empty
