3645efd8ba3fed58bc8874cec7c7df0c16840a65
Motin <motin@motin.eu>
2022-02-14 12:50:52 +0200

    Move dependency specs and test config into poetry project setup

--- pre-poetry-dependency-management/lint-fix.sh
+++
@@ -1,6 +0,0 @@
-set -x
-eval "$(conda shell.bash hook)"
-conda activate ./venvdev
-black --exclude 'venv|venvdev|.eggs' .
-python -m isort -rc --skip venv,venvdev .
-python -m autopep8 -r --in-place --exclude venv,venvdev .
---
+++ setup.py
@@ -0,0 +1,4 @@
+import setuptools
+
+if __name__ == "__main__":
+    setuptools.setup(name="selfquantifier")   # without the name the package is installed as UNKNOWN
